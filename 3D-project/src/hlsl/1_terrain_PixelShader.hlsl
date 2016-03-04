012882f63d998b3425ae1f2180b19d1e848b5444
sojunator <carl-pedal@hotmail.com>
2016-03-04 08:47:49 +0100

    forgot the output for the ps

--- 3D-project/src/hlsl/1_terrain_PixelShader.hlsl
+++ 3D-project/src/hlsl/1_terrain_PixelShader.hlsl
@@ -4,7 +4,20 @@ struct PixelInput
 	float4 color : COLOR;
 };
 
-float4 PS_main(PixelInput input) : SV_TARGET
+struct PixelOut
 {
-	return input.color;
+	float4 normal : SV_Target0;
+	float4 diffuse : SV_Target1;
+	float4 specular : SV_Target2;
+	float4 position : SV_Target3;
+};
+
+PixelOut PS_main(PixelInput input) : SV_TARGET
+{
+	PixelOut output;
+	output.position = input.position;
+	output.diffuse = input.color;
+	output.normal = float4(0.0f, 1.0f, 0.0f, 0.0f);
+	output.specular = input.position;
+	return output;
 }
